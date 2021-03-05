0595b1af85cd8477e37d805912488f0bb605ad2f
Steve <callasteven@gmail.com>
2021-03-05 11:02:20 -0700

    Add basic Playerjs

---
+++ Player.js
@@ -0,0 +1,26 @@
+class Player {
+  constructor() {
+    this.id = player-id;
+    this.token = {1: 'TBD', 5: 'TBD' };
+    this.wins = [];
+  }
+
+  saveToLocalStorage() {
+    //
+  }
+
+  retrieveFromLocalStorage() {
+    //
+  }
+
+}
+
+//PLAYER CLASS
+// PROPERTIES:
+// 1) id (ex: 'one'), 
+// 2) token (ex: '⭐️'), 
+// 3) wins (ex: [])
+
+// METHODS:
+// 1) saveWinsToStorage
+// 2) retrieveWinsFromStorage
